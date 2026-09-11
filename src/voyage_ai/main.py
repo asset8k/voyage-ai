f1aa81a2d77ac23124bbe9c2e23c875baf5a765f
Asset <asset8k@gmail.com>
2026-09-11 13:33:04 +0500

    Added the CORS

--- src/voyage_ai/main.py
+++ src/voyage_ai/main.py
@@ -2,6 +2,7 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from voyage_ai.auth.router import router as auth_router
 from voyage_ai.database import engine
@@ -21,6 +22,16 @@ async def lifespan(_app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router, prefix="/api")
 app.include_router(trips_router, prefix="/api")
